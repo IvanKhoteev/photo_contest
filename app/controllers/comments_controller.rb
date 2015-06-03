f4c227157355e008d2e428aa931f1d4b8aba0b9f
Ivan Khoteev <xxxmlhk@mail.ru>
2015-06-03 14:41:19 +0300

    Реализована система комментариев

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+    @photo_gallery = PhotoGallery.find(params[:photo_gallery_id])
+    @comment = @photo_gallery.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to root_path
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :user_id)
+    end
+
 end
